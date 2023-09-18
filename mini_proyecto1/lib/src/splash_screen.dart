b5c1fd8bbe597451a1ba3caee576bbbf8536dd4e
SaulRecio06 <saul.recio04@gmail.com>
2023-09-17 19:33:51 -0700

    mini_proyecto1

--- mini_proyecto1/lib/src/splash_screen.dart
+++ mini_proyecto1/lib/src/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practica4/src/home_page.dart';
+import 'package:mini_proyecto1/src/home_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
